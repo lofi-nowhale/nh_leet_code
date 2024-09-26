9c0d3763af7a89dafa94518cc360ee8d4b5b1328
Noelle Hemphill <noelle.hemphill@gmail.com>
2024-09-25 20:38:36 -0600

    add python start

--- turing_work/code_babble/python/python_bootcamp/bootcamp_1.py
+++ turing_work/code_babble/python/python_bootcamp/bootcamp_1.py
@@ -1,11 +1,67 @@
-# basic printing
-print('Hello World')
+# # basic printing
+# print('Hello World')
 
-# basic maths
+# # basic maths
 
-print(2-1)
-print(2+1)
-print(2*2)
-print(3/2)
+# print(2-1)
+# print(2+1)
+# print(2*2)
+# print(3/2)
 
-# modulo / mod operater = returns back the remainder after a division
+# # modulo / mod operater = returns back the remainder after a division
+# print(7 % 4) # 4 goes into 7 1 time with a remainder of 3
+# print(50 % 5) # great way to check if a number if even or odd, if the mod is EVEN, then we will get 0 else, we will get a remainder
+
+# # variable assignment
+# my_income = 100
+
+# tax_rate = 0.1 
+
+# my_taxes = my_income * tax_rate
+
+# print(my_taxes)
+
+
+# # strings - like ruby, you can use either single or double quotes
+# # slicing syntax [start:stop:step]
+# print("Hello World!")
+
+# #escape sequence '\n' will put this on a new line 
+
+# print("Hello \nworld")
+
+# #escape sequence '\t' will tab over anything after it 
+
+# print("Hello \tworld")
+
+# # len function this allows you to check the length of the string 
+# print(len("hello"))
+
+# # string indexing - grabbing 1 character | string slicing - grabbing a chunk/ subsection
+# my_string = "Hello World"
+
+# print(my_string[0])
+# print(my_string[-2])
+
+# new_string = "abcdefghij"
+# print(new_string[2:]) # goes all the way to the end 
+# print(new_string[:3]) # everything UP to a particular index - the STOP index ':3' go UP to but do not include
+
+# print(new_string[3:6]) # grabbing this subsection of def in the middle
+
+# print(new_string[1:3]) #grabbing another subsection example - 'bc'
+
+# print(new_string[2:5])
+
+# # looking at step size 
+
+# print(new_string[::3]) # go in jumps of 3
+# print(new_string[2:7:2]) 
+
+# print(new_string[::-1]) # this will reverse your string
+
+# name = "Noelle"
+# print(name)
+
+# name = "Bret"
+# print(name)
